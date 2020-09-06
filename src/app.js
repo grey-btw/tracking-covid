48939880d235994d6312519bb13322f0f46716e9
grey-btw <lehoangtrung2211@gmail.com>
2020-09-06 19:12:48 +0700

    update all

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+import coronaImage from './images/image.png';
+
+class App extends React.Component {
+    //this state used to bring data into component Cards
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState( { data: fetchedData });
+    }
+
+    handleCountryChange = async (country) => {
+        //fetch data
+        const fetchedData = await fetchData(country);
+        console.log(fetchedData); 
+
+        //set the state
+        this.setState( { data: fetchedData, country: country});
+    }
+
+
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={ styles.container }>
+                <img src={coronaImage} className={styles.image} alt='Corona'/>
+                <Cards data={ data } />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={ data } country={ country }/>
+            </div>  
+        );
+    }
+}
+
+export default App;
